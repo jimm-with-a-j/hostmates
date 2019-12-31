b7ba7dc81cf34b223cea7f1adcbb5ff457de2e68
Kitson <jpw.kitson@gmail.com>
2019-12-31 15:34:29 -0500

    added content type header to auth header list

--- config.py
+++ config.py
@@ -11,5 +11,8 @@ class Config:
         with open(CONFIG_FILE) as config_file:
             config = yaml.load(config_file, Loader=yaml.FullLoader)
             self.token = config["apiToken"]
+            self.tenant = config["tenant"]
+            self.auth_header = {'Authorization': 'Api-Token {token}'.format(token=self.token),
+                                'Content-Type': 'application/json'}
             self.delimiter = config["delimiter"]
             self.components = config["components"]
