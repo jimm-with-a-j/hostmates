0e891abd00f2d2c93d3b8c01ddc549957f21610f
Kitson <jpw.kitson@gmail.com>
2020-01-03 17:25:32 -0500

    working mz creation - added function to create condition json

--- hostmates.py
+++ hostmates.py
@@ -3,9 +3,11 @@ import json
 import requests
 from config import Config
 import itertools
+import copy
 
 TAG_ENDPOINT = "/api/config/v1/autoTags/"
 MZ_ENDPOINT = "/api/config/v1/managementZones/"
+
 
 class Main:
 
@@ -18,16 +20,28 @@ class Main:
         self.combined_management_zones = self.config.combined_management_zones
 
     def create_management_zones(self):
-        for combination in self.combined_management_zones:  # e.g. businessUnit_environment
+        for entry in self.combined_management_zones:  # e.g. businessUnit_environment
             arrays = []
-            for portion in combination['components']:  # looking for the values for that portion (e.g. component)
+            for tag in entry['components']:  # looking for the values for that tag (e.g. component)
                 for component in self.components:
-                    if component['name'] == portion:
+                    if component['name'] == tag:
                         arrays.append(component["mzValues"])  # adding to the array that holds each piece we need
 
-            combined = list(itertools.product(*arrays))  # creates list with all combinations
-            print(combined)
-
+            list_of_mzs = list(itertools.product(*arrays))  # creates list with all combinations
+            for mz in list_of_mzs:
+                with open("mz_template.json", "r") as template_json:
+                    mz_json = json.load(template_json)
+                i = 0
+                while i < len(mz):
+                    mz_json['rules'][0]['conditions'].append(create_condition_json(
+                        entry['components'][i], mz[i]))
+                    mz_json['rules'][1]['conditions'].append(create_condition_json(
+                        entry['components'][i], mz[i]))
+                    i = i + 1
+                mz_name = '_'.join(mz)
+                mz_json['name'] = mz_name
+                print("Creating {mz} management zone...".format(mz=mz_name))
+                self.post_request(self.tenant + MZ_ENDPOINT, mz_json)
 
     def create_tags(self):
         counter = 0
@@ -39,7 +53,7 @@ class Main:
 
             # if first entry in hostgroup
             if component['order'] == 1:
-                tag_json['rules'][0]['valueFormat'] = "{{HostGroup:Name/^([^{delimiter}]++)}}"\
+                tag_json['rules'][0]['valueFormat'] = "{{HostGroup:Name/^([^{delimiter}]++)}}" \
                     .format(delimiter=self.delimiter)
                 tag_json['rules'][1]['valueFormat'] = "{{HostGroup:Name/^([^{delimiter}]++)}}" \
                     .format(delimiter=self.delimiter)
@@ -50,7 +64,7 @@ class Main:
                 for _ in range(component['order'] - 1):
                     regex_start += "[^_]+_"
 
-                tag_json['rules'][0]['valueFormat'] = "{{HostGroup:Name/{regex_start}([^{delimiter}]++)}}"\
+                tag_json['rules'][0]['valueFormat'] = "{{HostGroup:Name/{regex_start}([^{delimiter}]++)}}" \
                     .format(delimiter=self.delimiter, regex_start=regex_start)
                 tag_json['rules'][1]['valueFormat'] = "{{HostGroup:Name/{regex_start}([^{delimiter}]++)}}" \
                     .format(delimiter=self.delimiter, regex_start=regex_start)
@@ -69,7 +83,7 @@ class Main:
     def post_request(self, target, payload):
         try:
             response = requests.post(target, headers=self.config.auth_header, data=json.dumps(payload))
-            assert(str(response.status_code).startswith("2"))
+            assert (str(response.status_code).startswith("2"))
         except AssertionError as e:
             print("Non 200 response from API Call")
             print(response.content)
@@ -77,6 +91,26 @@ class Main:
             print(response.status_code)
             return response
 
+
+def create_condition_json(key, value):
+    condition_json = \
+        {
+            "key": {
+                "attribute": "HOST_TAGS"
+            },
+            "comparisonInfo": {
+                "type": "TAG",
+                "operator": "EQUALS",
+                "value": {
+                    "context": "CONTEXTLESS",
+                    "key": key,
+                    "value": value
+                },
+                "negate": "false"
+            }
+        }
+
+    return condition_json
 
 if __name__ == '__main__':
     fire.Fire(Main)
